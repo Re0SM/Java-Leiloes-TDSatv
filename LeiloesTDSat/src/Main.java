bbb8d2afd90c50aa1e028e3442818d00e8adeec3
ReoSM <leosouza12m@gmail.com>
2024-01-08 21:10:09 -0300

    A primeira tela a abrir agora é cadastroVIEW

---
+++ LeiloesTDSat/src/Main.java
@@ -0,0 +1,10 @@
+/**
+ *
+ * @author ReoSM
+ */
+public class Main {
+    public static void main(String[] args) {
+        cadastroVIEW cadView = new cadastroVIEW();
+        cadView.setVisible(true);
+    }
+}
